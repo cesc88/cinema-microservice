84e51cbb3849352c6669bc62a9265a2b759384ff
cesc88 <cesc_peace@hotmail.com>
2020-07-03 23:11:45 -0300

    + more test repository.test

--- cinema-catalog-service/src/repository/repository.js
+++ cinema-catalog-service/src/repository/repository.js
@@ -9,7 +9,7 @@ function getAllCities(callback){
 }
 
 function getCinemasByCityId(cityId, callback){
-    var ObjCityId = ObjectId(cityId);
+    let ObjCityId = ObjectId(cityId);
     mongodb.connect((err, db) => {
         db.collection("cinemaCatalog").find({_id: ObjCityId},
             {cinemas:1}).toArray((err, cities) => {
@@ -19,8 +19,114 @@ function getCinemasByCityId(cityId, callback){
     });
 }
 
+function getMoviesByCinemaId(cinemaId, callback){
+    let ObjectCinemaId = ObjectId(cinemaId);
+    mongodb.connect((err, db) => {
+        db.collection("cineamCatalog").aggregate([
+            {$matches: {"cinema._id": ObjectCinemaId}},
+            {$unwind: "$cinema"},
+            {$unwind: "$cinema.salas"},
+            {$unwind: "$cinema.salas.sessoes"},
+            {$group: {_id:{
+                filme:"$cinema.salas.sessoes.filme", 
+                idFilme: "$cinema.salas.sessoes.idFilme"}}}
+        ]).toArray(callback)
+    });
+}
+
+function getMoviesByCityId(cityId, callback){
+    let objCityId = ObjectId(cityId);
+    mongodb.connect((err, db) => {
+        db.collection("cinemaCatalog").aggregate([
+            {$match: {"_id": objCityId}},
+            {$unwind: "$cinemas"},
+            {$unwind: "cinemas.salas"},
+            {$unwind: "cinemas.salas.sessoes"},
+            {$group: {_id: { 
+                filme: "$cinemas.salas.sessoes.filme", 
+                idFilme: "$cinemas.salas.sessoes.idFilme"}}}
+        ]).toArray((err, sessions) => {
+            if(err) return callback(err, null);
+            callback(err, sessions.map(item => {
+                return {idFilme: item._id.idFilme, 
+                    filme: item._id.filme
+            }}));
+        });
+    })
+}
+
+function getmovieSessionsByCityId(movieId, cityId, callback){
+    let objMovieId = ObjectId(movieId);
+    let objCityId = ObjectId(cityId);
+
+    mongodb.connect((err, db) => {
+        db.collection("cinemaCatalog").aggregate([
+            {$match: {objCityId}},
+            {$unwind: "$cinemas"},
+            {$unwind: "$cinemas.salas"},
+            {$unwind: "$cinemas.salas.sessoes"},
+            {$match: {"cinemas.salas.sessoes.idFilme":
+        objMovieId}},
+            {$group: {_id: {
+                filme:"$cinemas.salas.sessoes.filme", 
+                idFilme: "$cinemas.salas.sessoes.idFilme",
+                idCinema: "$cinemas._id", 
+                salas: "$cinemas.salas.nome", 
+                sessao: "$cinemas.salas.sessoes"}}}
+        ]).toArray((err, sessions) => {
+            if(err) return callback(err, null);
+            callback(err, sessions.map(item => {
+                return {
+                    idFilme: item._id.idFilme,
+                    filme: item._id.filme, 
+                    idCinema: item._id.idCinema,
+                    sala: item._id.sala,
+                    sessoa: item._id.sessao
+            }}));
+        });
+    });
+}
+
+function getMovieSessionsByCinemaId(movieId, cinemaId, callback){
+    let objMovieId = ObjectId(movieId);
+    let objCinemaId = ObjectId(cinemaId);
+    mongodb.connect((err, db) => {
+        db.collection("cinemaCatalog").aggregate([
+            {$match: {objCinemaId}},
+            {$unwind: "$cinemas"},
+            {$unwind: "$cinemas.salas"},
+            {$unwind: "$cinemas.salas.sessoes"},
+            {$match: {"cinemas.salas.sessoes.idFilme":
+        objMovieId}},
+            {$group: {_id: {
+                filme:"$cinemas.salas.sessoes.filme", 
+                idFilme: "$cinemas.salas.sessoes.idFilme",
+                idCinema: "$cinemas._id", 
+                salas: "$cinemas.salas.nome", 
+                sessao: "$cinemas.salas.sessoes"}}}
+        ]).toArray((err, sessions) => {
+            if(err) return callback(err, null);
+            callback(err, sessions.map(item => {
+                return {
+                    idFilme: item._id.idFilme,
+                    filme: item._id.filme, 
+                    idCinema: item._id.idCinema,
+                    sala: item._id.sala,
+                    sessoa: item._id.sessao
+             }}));
+        });    
+    })
+}
+
 function disconnect(){
     return mongodb.disconnect();
 }
 
-module.exports = { getAllCities, getCinemasByCityId, disconnect }
+module.exports = { 
+    getAllCities, 
+    getCinemasByCityId, 
+    getMoviesByCinemaId, 
+    getMoviesByCityId,
+    getmovieSessionsByCityId,
+    getMovieSessionsByCinemaId,  
+    disconnect }
--- movies-service/src/repository/repository.test.js
+++ movies-service/src/repository/repository.test.js
@@ -36,6 +36,35 @@ function runTest(){
         });
     })
 
+    test('Repository getMoviesByCinemaId', (t) => {
+        repository.getMoviesByCinemaId(cinemaId, (err, movies) => {
+            t.assert(!err && movies && movies.length > 0, "Movies By Cinemas Id Returned");
+            t.end();
+        });
+    })
+
+    test('Repository getMoviesByCityId', (t) => {
+        repository.getMoviesByCityId(cityId, (err, movies) => {
+            if(movies && movies.length > 0) movieId = movies[0]._idFilme;
+            t.assert(!err && movies && movies.length > 0, "Movies By City Id Returned");
+            t.end();
+        })
+    })
+
+    test('Repository getMoviesSessionsByCityId', (t) => {
+        repository.getMoviesSessionsByCityId(movieId, cityId, (err, sessions) => {
+            t.assert(!err && sessions && sessions.length > 0, "Movie Sessions By City Id Returned");
+            t.end();
+        });
+    })
+
+    test('Repository getMovieSessionsByCinemaId', (t) => {
+        repository.getMoviesSessionsByCinemaId(movieId, cinemaId, (err, sessions) => {
+            t.assert(!err && sessions && sessions.length > 0, "Movie Sessions By Cinema Id Returned");
+            t.end();
+        });
+    })
+
     test('Repository Disconnect', (t) => {
         t.assert(repository.disconnect(), "Disconnect Ok");
         t.end();
