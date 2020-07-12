f50178178296495e3f31f18ef366bf5c5b3e97c9
cesc88 <cesc_peace@hotmail.com>
2020-07-12 20:54:10 -0300

    route login + URLs.js

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,3 +1,5 @@
+require("dotenv-safe").config();
+var jwt = require('jsonwebtoken')
 var http = require('http')
 const express = require('express')
 const httpProxy = require('express-http-proxy')
@@ -5,22 +7,56 @@ const app = express()
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 const helmet = require('helmet')
-const { USERS_API_URL, PRODUCTS_API_URL } = require('./URLs')
+const { USERS_API_URL, PRODUCTS_API_URL } = require('./URLs');
+
 
 const userServiceProxy = httpProxy(USERS_API_URL)
 const productsServiceProxy = httpProxy(PRODUCTS_API_URL)
+
+
+function verifyJWT(req, res, next){
+    var token = req.headers['x-acess-token'];
+    if(!token) return res.status(401)
+        .send({ auth:false, message: 'No token provided.'});
+
+        jwt.verify(token, process.env.SECRET,
+            function(err, decoded){
+                if(err) return res.status(500)
+                    .send({auth:false, message: 'Failed to authenticate token'});
+
+                    req.userId = decoded.id;
+                    next();
+            });
+}
 
 //Proxy request
 
 app.get('/', (req, res) => res.send("Welcome Gateway API"))
 
-app.get('/movies', (req, res, next) => 
+app.get('/movies', verifyJWT,(req, res, next) => 
     userServiceProxy(req, res, next)
 )
 
-app.get('/cities', (req, res, next) => 
+app.get('/cities', verifyJWT,(req, res, next) => 
     productsServiceProxy(req, res, next)
 )
+
+app.get('/logout', (req, res) => {
+    res.status(200).send({ auth:false, token:null });
+})
+
+app.post('/login', (req, res, next) => {
+    if(req.body.user === 'username' && req.body.password === 'password'){
+        const id = 1;
+        let token = jwt.sign({id}, process.env.SECRET,
+    {
+        expiresIn: 300
+    });
+    res.status(200).send({ auth: true, token: token
+    });
+    }
+    res.status(500).send('Invalid login')
+})
 
 app.use(logger('dev'));
 app.use(helmet());
