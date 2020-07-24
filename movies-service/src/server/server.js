962ddf6601600488a79fbd328cc91a938d5a48c5
cesc88 <cesc_peace@hotmail.com>
2020-07-23 22:31:39 -0300

    + Updates packages and server

--- movies-service/src/server/server.js
+++ movies-service/src/server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const cors = require('cors')
 var server = null;
 
 function start(api, repository, callback){
 
     const app = express();
+    app.use(cors());
     app.use(morgan('dev'));
     app.use(helmet());
     app.use((err, req, res, next) => {
